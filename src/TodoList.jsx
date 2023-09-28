236ad32c1118eca255a1dd49a1cd013ad743641b
Iuliia <ybe2004ybe@gmail.com>
2023-09-28 19:08:52 +0300

    delete function plus impoved css

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -22,47 +22,68 @@ function TodoList() {
     setDate(""); //clear the date input
   };
 
+  const deleteTodo = (index) => {
+    //Use filter() method to delete one item from the array todos.filter((todo, i) => i !== index)
+    const updatedTodos = todos.filter((todo, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  const clearTodos = () => {
+    // Clear all todos by setting an empty array
+    setTodos([]);
+  };
+
   return (
     <>
-      <h1 style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>
-        Todo List
-      </h1>
-      <div class="input-area">
-        <input
-          type="text"
-          name="desc"
-          onChange={inputChanged}
-          value={desc}
-          placeholder="Description"
-          style={{ marginRight: "10px", height: "30px" }}
-        />
-        <input
-          type="text"
-          name="date"
-          onChange={inputChanged}
-          value={date}
-          placeholder="Date"
-          style={{ marginLeft: "10px", height: "30px" }}
-        />
-        <button onClick={addTodo}>Add</button>
-      </div>
+      <div className="content-container">
+        <h1 style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>
+          Todo List
+        </h1>
+        <div class="input-area">
+          <input
+            type="text"
+            name="desc"
+            onChange={inputChanged}
+            value={desc}
+            placeholder="Description"
+            style={{ marginRight: "10px", height: "30px" }}
+          />
+          <input
+            type="text"
+            name="date"
+            onChange={inputChanged}
+            value={date}
+            placeholder="Date"
+            style={{ marginLeft: "10px", height: "30px" }}
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo, index) => (
-            <tr key={index}>
-              <td>{todo.description}</td>
-              <td>{todo.date}</td>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Date</th>
+              <th class="delete-btn">
+                <button onClick={clearTodos} style={{ width: "80%" }}>
+                  Clear
+                </button>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {todos.map((todo, index) => (
+              <tr key={index}>
+                <td>{todo.description}</td>
+                <td>{todo.date}</td>
+                <td class="delete-btn">
+                  <button onClick={() => deleteTodo(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
